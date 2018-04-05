8ada6ab1e85cf72a1c60b7d7cb4f6b3beaa31f33
peiper <niklas.peiper@gmail.com>
2018-04-05 16:16:00 +0200

    import styles.css

--- src/Main.jsx
+++ src/Main.jsx
@@ -6,6 +6,8 @@ import 'normalize.css';
 import store from './store/StoreCreator.dev';
 import App from './views/App';
 
+import './styles.css';
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
